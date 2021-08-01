cd07935f14026f92d1b8973557dfbc9ec50e0352
Sergio <colombia00028@gmail.com>
2021-08-01 11:10:34 -0500

    Añadiendo Swing, JFrame y logic

---
+++ src/calculationtriangle/CalculationTriangle.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculationtriangle;
+
+/**
+ *
+ * @author sevar
+ */
+public class CalculationTriangle {
+
+
+    public static void main(String[] args) {
+           
+       Triangle disable = new Triangle();
+                
+       disable.setLocationRelativeTo(null);
+       disable.setTitle("Interfaz Gráfica del Applet");
+       disable.setVisible(true);
+    }
+    
+}
